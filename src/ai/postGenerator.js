961f0ba6524bcf2a0dba7b1cdeb76aeca73c16b5
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-26 16:19:48 +0530

    Update postGenerator.js

--- src/ai/postGenerator.js
+++ src/ai/postGenerator.js
@@ -1,1 +1,23 @@
+const {askGemini}=require("./gemini");
 
+const {
+
+postPrompt
+
+}=require("./promptTemplates");
+
+async function generatePost(topic){
+
+return await askGemini(
+
+postPrompt(topic)
+
+);
+
+}
+
+module.exports={
+
+generatePost
+
+};
