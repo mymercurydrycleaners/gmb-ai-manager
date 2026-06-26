f37ba290bb8a69294e199c21404961fbdb976568
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-26 16:05:07 +0530

    Update validator.js

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,1 +1,25 @@
+const required = [
 
+"GEMINI_API_KEY"
+
+];
+
+function validate(){
+
+const missing=[];
+
+required.forEach(key=>{
+
+if(!process.env[key]){
+
+missing.push(key);
+
+}
+
+});
+
+return missing;
+
+}
+
+module.exports={validate};
