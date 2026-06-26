63f132498da0b40c76bd2f930b3cb0b6f012480f
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-26 15:52:29 +0530

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,1 +1,29 @@
+const { createLogger, transports, format } = require("winston");
 
+const logger = createLogger({
+
+level: "info",
+
+format: format.combine(
+
+format.timestamp(),
+
+format.simple()
+
+),
+
+transports: [
+
+new transports.Console(),
+
+new transports.File({
+
+filename: "logs/app.log"
+
+})
+
+]
+
+});
+
+module.exports = logger;
