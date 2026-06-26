fdf022a7b3cd5d2fab45381ec0d28848d03474d2
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-26 16:21:23 +0530

    Update post.js

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,1 +1,57 @@
+const express=require("express");
 
+const router=express.Router();
+
+const {
+
+generatePost
+
+}=require("../ai/postGenerator");
+
+router.get("/",async(req,res)=>{
+
+try{
+
+const topic=req.query.topic;
+
+if(!topic){
+
+return res.status(400).json({
+
+success:false,
+
+message:"Missing topic"
+
+});
+
+}
+
+const post=
+
+await generatePost(topic);
+
+res.json({
+
+success:true,
+
+post
+
+});
+
+}
+
+catch(err){
+
+res.status(500).json({
+
+success:false,
+
+message:err.message
+
+});
+
+}
+
+});
+
+module.exports=router;
