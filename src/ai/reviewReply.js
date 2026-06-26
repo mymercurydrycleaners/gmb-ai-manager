256a159d830ccb60c15b40950d68198575d7eb01
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-26 16:19:09 +0530

    Update reviewReply.js

--- src/ai/reviewReply.js
+++ src/ai/reviewReply.js
@@ -1,1 +1,23 @@
+const {askGemini}=require("./gemini");
 
+const {
+
+reviewPrompt
+
+}=require("./promptTemplates");
+
+async function generateReply(review){
+
+return await askGemini(
+
+reviewPrompt(review)
+
+);
+
+}
+
+module.exports={
+
+generateReply
+
+};
