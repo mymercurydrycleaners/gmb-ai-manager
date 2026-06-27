8ccb73517aa6fa84d2c565c4e86469f64d7223c8
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-27 18:05:23 +0530

    Update replies.js

--- src/google/replies.js
+++ src/google/replies.js
@@ -1,1 +1,42 @@
+const { google } = require("googleapis");
 
+async function replyToReview(reviewName, replyText) {
+  try {
+    const oauth2Client = new google.auth.OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET
+    );
+
+    oauth2Client.setCredentials({
+      refresh_token: process.env.GOOGLE_REFRESH_TOKEN
+    });
+
+    const business = google.mybusiness({
+      version: "v4",
+      auth: oauth2Client
+    });
+
+    const response = await business.accounts.locations.reviews.updateReply({
+      name: reviewName,
+      requestBody: {
+        comment: replyText
+      }
+    });
+
+    return {
+      success: true,
+      reply: response.data
+    };
+
+  } catch (err) {
+    return {
+      success: false,
+      error: err.message,
+      details: err.response?.data || null
+    };
+  }
+}
+
+module.exports = {
+  replyToReview
+};
