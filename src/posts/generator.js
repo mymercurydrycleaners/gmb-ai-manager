cfbf9c427131e4363ef45340cebea091d5b9c8ec
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-28 15:39:48 +0530

    Update generator.js

--- src/posts/generator.js
+++ src/posts/generator.js
@@ -1,1 +1,30 @@
+const templates = require("./templates");
 
+function randomItem(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generatePost(type = "tips") {
+
+  switch (type) {
+
+    case "offers":
+      return randomItem(templates.offers);
+
+    case "festivals":
+      return randomItem(templates.festivals);
+
+    case "announcements":
+      return randomItem(templates.announcements);
+
+    case "tips":
+    default:
+      return randomItem(templates.tips);
+
+  }
+
+}
+
+module.exports = {
+  generatePost
+};
