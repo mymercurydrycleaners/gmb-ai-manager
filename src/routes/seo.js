bba64c13c5b15b311455f92ef930a9fe5d0a8312
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-28 19:22:48 +0530

    Update seo.js

--- src/routes/seo.js
+++ src/routes/seo.js
@@ -1,1 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { runSeoAudit } = require("../seo/audit");
+
+router.get("/", async (req, res) => {
+
+  const result = await runSeoAudit({});
+
+  res.json(result);
+
+});
+
+module.exports = router;
 
