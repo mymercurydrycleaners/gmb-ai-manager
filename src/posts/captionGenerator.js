75430d5f9d9adf711f9921d94f2d44881047e6f0
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-28 21:09:11 +0530

    Create captionGenerator.js

---
+++ src/posts/captionGenerator.js
@@ -0,0 +1,18 @@
+function generateCaption(post) {
+
+  return `${post}
+
+✨ Visit My Mercury Dry Cleaners today.
+
+📍 Professional Dry Cleaning
+
+🧺 Premium Fabric Care
+
+☎ Contact us today.`;
+
+}
+
+module.exports = {
+  generateCaption
+};
+
