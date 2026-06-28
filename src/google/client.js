dd8979840b9c5195c2ab5c7b3e6391ea91e6a48b
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-28 09:53:27 +0530

    Update client.js

--- src/google/client.js
+++ src/google/client.js
@@ -1,1 +1,12 @@
+const { google } = require("googleapis");
 
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.GOOGLE_REFRESH_TOKEN
+});
+
+module.exports = oauth2Client;
