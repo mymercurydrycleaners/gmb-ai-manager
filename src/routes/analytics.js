6ac447d4ee5d09f40d5f34230fd53f882d16fa67
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-28 09:01:34 +0530

    Update analytics.js

--- src/routes/analytics.js
+++ src/routes/analytics.js
@@ -1,1 +1,15 @@
+const express = require("express");
 
+const router = express.Router();
+
+const { getAnalytics } = require("../dashboard/analytics");
+
+router.get("/", async (req, res) => {
+
+  const data = await getAnalytics();
+
+  res.json(data);
+
+});
+
+module.exports = router;
