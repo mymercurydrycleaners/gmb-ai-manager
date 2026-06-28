fcd98ef3938da066e1f1f2b37781b802a3011dbd
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-28 19:53:30 +0530

    Update reviewCache.js

--- src/reviews/reviewCache.js
+++ src/reviews/reviewCache.js
@@ -1,1 +1,14 @@
+const cache = new Set();
 
+function isDuplicate(reviewId) {
+  return cache.has(reviewId);
+}
+
+function saveReview(reviewId) {
+  cache.add(reviewId);
+}
+
+module.exports = {
+  isDuplicate,
+  saveReview
+};
