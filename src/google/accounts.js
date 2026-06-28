87e1718578bca705eb49b7b93474174bd2a9057f
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-28 09:55:18 +0530

    Update accounts.js

--- src/google/accounts.js
+++ src/google/accounts.js
@@ -1,1 +1,15 @@
+const oauth2Client = require("./client");
 
+async function getAccounts() {
+
+  const response = await oauth2Client.request({
+    url: "https://mybusinessaccountmanagement.googleapis.com/v1/accounts"
+  });
+
+  return response.data;
+
+}
+
+module.exports = {
+  getAccounts
+};
