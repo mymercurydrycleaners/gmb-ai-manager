59a246fc5b252d74f37f6b34e0c7a47a9c2839b8
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-28 19:26:36 +0530

    Update scoring.js

--- src/seo/scoring.js
+++ src/seo/scoring.js
@@ -1,1 +1,32 @@
+function calculateSEO(data = {}) {
 
+  let score = 100;
+
+  if (!data.description)
+    score -= 10;
+
+  if (!data.services || data.services.length < 5)
+    score -= 10;
+
+  if (!data.photos || data.photos.length < 10)
+    score -= 10;
+
+  if (!data.posts || data.posts.length < 8)
+    score -= 10;
+
+  if (!data.faqs || data.faqs.length < 5)
+    score -= 10;
+
+  if (!data.reviews || data.reviews.length < 20)
+    score -= 10;
+
+  if (score < 0)
+    score = 0;
+
+  return score;
+
+}
+
+module.exports = {
+  calculateSEO
+};
