90db434981a02819c3cf442cfdeea444335dbf04
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-28 15:42:31 +0530

    Update publisher.js

--- src/posts/publisher.js
+++ src/posts/publisher.js
@@ -1,1 +1,22 @@
+async function publishPost(post) {
 
+  console.log("==================================");
+  console.log("Google Business Post");
+  console.log("==================================");
+
+  console.log(post.title);
+  console.log(post.body);
+
+  console.log("==================================");
+
+  return {
+    success: true,
+    message: "Post Ready",
+    post
+  };
+
+}
+
+module.exports = {
+  publishPost
+};
