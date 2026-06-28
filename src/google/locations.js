542ebb506c4195c2843d8d586bde038208584817
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-28 13:45:41 +0530

    Update locations.js

--- src/google/locations.js
+++ src/google/locations.js
@@ -1,1 +1,48 @@
+```javascript
+const oauth2Client = require("./client");
+const { getAccounts } = require("./accounts");
 
+async function getLocations() {
+
+  try {
+
+    const accounts = await getAccounts();
+
+    if (
+      !accounts.accounts ||
+      accounts.accounts.length === 0
+    ) {
+      return {
+        success: false,
+        message: "No Business Account Found"
+      };
+    }
+
+    const account = accounts.accounts[0];
+
+    const response = await oauth2Client.request({
+      url: `https://mybusinessbusinessinformation.googleapis.com/v1/${account.name}/locations`
+    });
+
+    return {
+      success: true,
+      account: account.name,
+      locations: response.data.locations || []
+    };
+
+  } catch (err) {
+
+    return {
+      success: false,
+      error: err.message,
+      details: err.response?.data || null
+    };
+
+  }
+
+}
+
+module.exports = {
+  getLocations
+};
+```
