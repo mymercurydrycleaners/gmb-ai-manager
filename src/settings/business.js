07d461dd78899dca338bdc544447c83e66f122ce
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-28 21:44:48 +0530

    Create business.js

---
+++ src/settings/business.js
@@ -0,0 +1,24 @@
+const business = {
+
+  name: "My Mercury Dry Cleaners",
+
+  phone: "+91XXXXXXXXXX",
+
+  email: "info@mymercurydrycleaners.com",
+
+  website: "",
+
+  address: "",
+
+  workingHours: "9:00 AM - 9:00 PM"
+
+};
+
+function getBusinessSettings() {
+  return business;
+}
+
+module.exports = {
+  getBusinessSettings
+};
+
