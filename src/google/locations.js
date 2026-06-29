2d31bfe9ecbd7b541192d19b919809c9d0b1104f
mymercurydrycleaners <mdcmahoba01@gmail.com>
2026-06-29 09:11:12 +0530

    Update locations.js

--- src/google/locations.js
+++ src/google/locations.js
@@ -1,40 +1,35 @@
-const oauth2Client = require("./client");
-const { getAccounts } = require("./accounts");
+const {
+  businessInfo
+} = require("./services");
 
-async function getLocations() {
+async function getLocations(account) {
 
   try {
 
-    const accounts = await getAccounts();
+    const response =
+      await businessInfo.accounts.locations.list({
 
-    if (
-      !accounts.accounts ||
-      accounts.accounts.length === 0
-    ) {
-      return {
-        success: false,
-        message: "No Business Account Found"
-      };
-    }
+        parent: account
 
-    const account = accounts.accounts[0];
-
-    const response = await oauth2Client.request({
-      url: `https://mybusinessbusinessinformation.googleapis.com/v1/${account.name}/locations`
-    });
+      });
 
     return {
+
       success: true,
-      account: account.name,
-      locations: response.data.locations || []
+
+      locations:
+        response.data.locations || []
+
     };
 
   } catch (err) {
 
     return {
+
       success: false,
-      error: err.message,
-      details: err.response?.data || null
+
+      error: err.message
+
     };
 
   }
